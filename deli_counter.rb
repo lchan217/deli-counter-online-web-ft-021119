868a0ebc0ad29a39392f685836d2d9518db19783
Lizzie Chan <lchan217@gmail.com>
2019-03-06 01:23:12 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,39 +1,27 @@
-def line(katz_deli)
-  if katz_deli.length == 0
-    puts "The line is currently empty."
-    return
-  else
-    new_array = String.new
-    katz_deli.each_with_index do |person, index|
-      new_array << (" #{index+1}. #{person}")
+  def line(katz_deli)
+    if katz_deli.length == 0
+      puts "The line is currently empty."
+      return 
+    else
+      new_string = String.new
+      katz_deli.each_with_index do |person, index|
+        new_string << (" #{index+1}. #{person}")
+      end
     end
+    puts "The line is currently:"+"#{new_string}"
   end
-  puts "The line is currently:"+"#{new_array}"
-end
-
-# def line(katz_deli)
-#   if katz_deli.length == 0 
-#     puts "The line is currently empty."
-#   else 
-#     new_array = [] 
-#     katz_deli.each_with_index do |name,index|
-#       new_array << "#{index+1}. #{name}"
-#       new_array.join
-#     end 
-#     puts "The line is currently: " + "#{new_array}"
-#   end 
-# end 
-
-def take_a_number(katz_deli, name)
-    katz_deli<< name 
-   puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
-end 
-
-def now_serving(katz_deli)
-  if katz_deli.length == 0 
-    puts "There is nobody waiting to be served!"
-  else 
-    puts "Currently serving #{katz_deli[0]}."
-    katz_deli.shift
+  
+  def take_a_number(katz_deli, name)
+    katz_deli << name 
+    puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
   end 
-end 
+  
+  def now_serving(katz_deli)
+    if katz_deli.length == 0 
+      puts "There is nobody waiting to be served!"
+    else 
+      puts "Currently serving #{katz_deli[0]}."
+      katz_deli.shift
+    end 
+  end 	
+  
