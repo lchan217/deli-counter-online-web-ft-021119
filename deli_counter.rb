241744091853067d0e72ecbab002db6a2730314a
Lizzie Chan <lchan217@gmail.com>
2019-03-06 01:25:32 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,5 +1,9 @@
 def line(katz_deli)
   if katz_deli.length == 0 
     puts "The line is currently empty."
+    elsif katz_deli > 0 
+      katz_deli.each_with_index do |person, index|
+        puts "The line is currently: #{index+1}. #{person}"
+    end 
   end 
 end 
