a01ffbb90e64c375f9e2ecc8badc9db5f1beae40
Lizzie Chan <lchan217@gmail.com>
2019-03-06 01:29:43 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,10 +2,9 @@ def line(katz_deli)
   if katz_deli.length == 0 
     puts "The line is currently empty."
     elsif katz_deli.length > 0 
-      new_array = []
+      the_line = 
       katz_deli.each_with_index do |person, index|
        new_array << "#{index+1}. #{person} "
       end 
-      puts "The line is currently: #{new_array.join}"
   end 
 end 
