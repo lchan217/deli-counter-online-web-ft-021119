ff30370fe7f7302df96d9aea34cf02e09a314a0e
Lizzie Chan <lchan217@gmail.com>
2019-03-06 01:35:05 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -9,18 +9,18 @@ def line(katz_deli)
       puts "The line is currently:#{new_array.join}"
   end 
 end 
-
-def take_a_number(katz_deli, name)
+  
+  def take_a_number(katz_deli, name)
     katz_deli << name 
     puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
+  end 
   
-end 
-
-def now_serving(katz_deli)
-  if katz_deli.length == 0 
-    puts "There is nobody waiting to be served!"
-  else 
-    puts "Currently serving #{katz_deli[0]}."
-    katz_deli.shift
-  end
-end 
+  def now_serving(katz_deli)
+    if katz_deli.length == 0 
+      puts "There is nobody waiting to be served!"
+    else 
+      puts "Currently serving #{katz_deli[0]}."
+      katz_deli.shift
+    end 
+  end 	
+  
