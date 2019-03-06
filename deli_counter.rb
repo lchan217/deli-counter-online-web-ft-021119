8289331054bae2753b352fc4b762c98a088abb4b
Lizzie Chan <lchan217@gmail.com>
2019-03-06 01:26:53 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,8 +2,10 @@ def line(katz_deli)
   if katz_deli.length == 0 
     puts "The line is currently empty."
     elsif katz_deli.length > 0 
+      new_array = []
       katz_deli.each_with_index do |person, index|
-         "The line is currently: #{index+1}. #{person}"
-    end 
+       new_array << "#{index+1}. #{person}"
+      end 
+      puts "The line is currently: #{new_array}"
   end 
 end 
